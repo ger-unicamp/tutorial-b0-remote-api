9800cac1738dc5d2ff8193a99a6ce1f29e2664b3
Natan Rodrigues de Oliveira <natanrodriguesdeoliveira@gmail.com>
2021-05-24 20:13:05 -0300

    Build feito para o Windows. Padronizar o caminho C:\dev\boost_176_0

--- build.bat
+++ build.bat
@@ -10,7 +10,7 @@ echo %COPPELIA_PROGRAMMING_DIR%
 
 set COMPILER_FLAGS=/W3 /O1 /EHsc /c /std:c++17
 set COMPILER_DEFINES=/D "WIN32" /D "_WINDOWS" /D "NDEBUG"
-set COMPILER_INCLUDES=/I%COPPELIA_PROGRAMMING_DIR%\blueZero\include\b0\bindings /I%COPPELIA_PROGRAMMING_DIR%\b0RemoteApiBindings\cpp /I%COPPELIA_PROGRAMMING_DIR%\b0RemoteApiBindings\cpp\msgpack-c\include /I"C:\local\boost_1_76_0"
+set COMPILER_INCLUDES=/I%COPPELIA_PROGRAMMING_DIR%\blueZero\include\b0\bindings /I%COPPELIA_PROGRAMMING_DIR%\b0RemoteApiBindings\cpp /I%COPPELIA_PROGRAMMING_DIR%\b0RemoteApiBindings\cpp\msgpack-c\include /I"C:\dev\boost_1_76_0"
 set COMPILERSTRING=%COMPILER_INCLUDES% %COMPILER_DEFINES% %COMPILER_FLAGS%
 
 REM  b0.lib is normally located in the same folder as coppeliaSim.exe
